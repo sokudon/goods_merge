51e695a0dc02444b9533c6ecc827c717dd6694df
sokudon <tdty15@gmail.com>
2024-01-21 13:01:01 +0900

    きょうう

---
+++ MFCマージツール/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace MFCマージツール
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "統合";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(493, 94);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 127);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(492, 77);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(374, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 269);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GOODS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MFCマージツール/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MFCマージツール
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            //コントロール内にドロップされたとき実行される
+            //ドロップされたすべてのファイル名を取得する
+            string[] fileName =
+                (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ListBoxに追加する
+            listBox1.Items.AddRange(fileName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] mfc = new string[4];
+            for (var i = 0; i < 4; i++)
+            {
+                mfc[i] = listBox1.Items[i].ToString();
+            }
+
+            string[] mfcs = new string[4];
+            for (var k = 0; k < 4; k++)
+            {
+                if (Regex.IsMatch(mfc[k], "GOODS.csv"))
+                {
+                    mfcs[0] = mfc[k];
+                }
+                if (Regex.IsMatch(mfc[k], "GOODS_UNI.csv"))
+                {
+                    mfcs[1] = mfc[k];
+                }
+                if (Regex.IsMatch(mfc[k], "GOODS_MAI.csv"))
+                {
+                    mfcs[2] = mfc[k];
+                }
+                if (Regex.IsMatch(mfc[k], "GOODS_ONG.csv"))
+                {
+                    mfcs[3] = mfc[k];
+                }
+            }
+
+            string mfc_base = mfcs[0];
+            string mfc_hg = mfcs[1];//uni
+            string mfc_new = mfcs[2];//mai
+            string mfc_std = mfcs[3];//ong
+            string MFC = "";
+            int count = 0;
+            int counthg = 0;
+            int countnew = 0;
+            int countstd = 0;
+            List<string> lists = new List<string>();
+            List<string> listshg = new List<string>();
+            List<string> listsnew = new List<string>();
+            List<string> listsstd = new List<string>();
+
+            StreamReader sr = new StreamReader(mfcs[0]);
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    lists.Add(line);
+                    MFC = MFC + line + "\r\n";
+                    count++;
+                }
+
+
+            }
+            StreamReader srr = new StreamReader(mfcs[1]);
+            {
+                while (!srr.EndOfStream)
+                {
+                    string line = srr.ReadLine();
+                    listshg.Add(line);
+                    counthg++;
+                }
+
+
+            }
+            StreamReader srrr = new StreamReader(mfcs[2]);
+            {
+                while (!srrr.EndOfStream)
+                {
+                    string line = srrr.ReadLine();
+                    listsnew.Add(line);
+                    countnew++;
+                }
+
+
+            }
+            StreamReader srrrr = new StreamReader(mfcs[3]);
+            {
+                while (!srrrr.EndOfStream)
+                {
+                    string line = srrrr.ReadLine();
+                    listsstd.Add(line);
+                    countstd++;
+                }
+
+
+            }
+
+            for(var i = 1; i < count; i++)
+            {
+                string temp = lists[i];
+                string tmp = lists[i].Replace("+","\\+");
+
+
+                for (var k = 1; k < counthg; k++)
+                {
+                    string tmphg = listshg[k];
+                    if (Regex.IsMatch(tmphg,tmp))
+                    {
+                        MFC = MFC.Replace(temp, tmphg+" UNI");
+
+                    }
+
+
+
+                }
+                for (var k = 1; k < countnew; k++)
+                {
+                    string tmphg = listsnew[k];
+                    if (Regex.IsMatch(tmphg, tmp))
+                    {
+                        MFC = MFC.Replace(temp, tmphg + " MAI");
+
+                    }
+
+
+
+                }
+                for (var k = 1; k < countstd; k++)
+                {
+                    string tmphg = listsstd[k];
+                    if (Regex.IsMatch(tmphg, tmp))
+                    {
+                        MFC = MFC.Replace(temp, tmphg + " ONG");
+
+                    }
+
+
+
+                }
+
+                for (var k = 1; k < count; k++)
+                {
+                    string tmphg = lists[k];
+                    if (Regex.IsMatch(tmphg, tmp))
+                    {
+                        MFC = MFC.Replace(temp, tmphg + ",");
+
+                    }
+
+
+
+                }
+
+
+            }
+
+            textBox1.Text = MFC.Replace("data-longitude,", "data-longitude,cabinet");
+
+
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string data = "";
+            string name = listBox1.Items[0].ToString();
+            MatchCollection mc = Regex.Matches(name, @"\d+MFC");
+
+            if (mc.Count > 0) { }
+            //data = mc[0].Value;
+
+
+            File.WriteAllText(@data+"_merged.csv", textBox1.Text);
+        }
+        }
+    }
